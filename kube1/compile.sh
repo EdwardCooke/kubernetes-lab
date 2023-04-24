6c8f90a45716267a374d05cdca1536140261eb78
Edward Cooke <edward@frakkingsweet.com>
2023-04-24 14:49:43 -0600

    Refactor kube1

--- kube1/compile.sh
+++ kube1/compile.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir -p build
+
 echo "#!/bin/bash
 
 echo '$(cat ../ca/ca.crt)' > /usr/local/share/ca-certificates/k8slan.crt
@@ -7,7 +9,9 @@ echo '$(cat ../ca/wildcard.crt)' > /etc/ssl/private/wildcard.crt
 echo '$(cat ../ca/wildcard.key)' > /etc/ssl/private/wildcard.key
 
 update-ca-certificates
-" > certs.sh
+" > build/certs.sh
 
-sed "s\\__authorizedkey__\\$(cat ~/.ssh/vm.pub)\\" user-data > cidata/user-data.source
-cloud-init devel make-mime -a cidata/user-data.source:cloud-config -a certs.sh:x-shellscript-per-once --force > cidata/user-data
+sed "s\\__authorizedkey__\\$(cat ~/.ssh/vm.pub)\\" user-data > build/user-data.source
+cloud-init devel make-mime -a build/user-data.source:cloud-config -a build/certs.sh:x-shellscript-per-once --force > cidata/user-data
+cp meta-data cidata
+cp network-config cidata
