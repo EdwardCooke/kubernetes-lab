e5c4eca59b081070426125a4bd456fe9037035cb
Edward Cooke <edward@frakkingsweet.com>
2023-04-30 22:41:44 -0600

    Change destroy to shutdown in stop.sh

--- stop.sh
+++ stop.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-virsh destroy dns  
-virsh destroy kube1
-virsh destroy kube2
-virsh destroy kube1cp1
-virsh destroy kube1cp2
-virsh destroy kube1cp3
-virsh destroy kube1w1
-virsh destroy kube1w2
-virsh destroy kube2cp1
-virsh destroy kube2cp2
-virsh destroy kube2cp3
-virsh destroy kube2w1
-virsh destroy kube2w2
+virsh shutdown dns
+virsh shutdown kube1
+virsh shutdown kube2
+virsh shutdown kube1cp1
+virsh shutdown kube1cp2
+virsh shutdown kube1cp3
+virsh shutdown kube1w1
+virsh shutdown kube1w2
+virsh shutdown kube2cp1
+virsh shutdown kube2cp2
+virsh shutdown kube2cp3
+virsh shutdown kube2w1
+virsh shutdown kube2w2
