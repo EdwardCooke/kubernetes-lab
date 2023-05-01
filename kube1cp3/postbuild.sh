c1abce7bd3fd6cf19f0763650d21454d33f1c0e9
Edward Cooke <edward@frakkingsweet.com>
2023-04-30 20:35:49 -0600

    Re org kube1w1 and kube1w2

--- kube1cp3/postbuild.sh
+++ kube1cp3/postbuild.sh
@@ -2,13 +2,13 @@
 echo "Waiting for the host to come up"
 
 cancelled=false
-until echo -n "."; ping -c1 kube1cp2.k8s.lan >/dev/null 2>&1; do :; done &
+until echo -n "."; ping -c1 kube1cp3.k8s.lan >/dev/null 2>&1; do :; done &
 trap "kill $!; cancelled=true" SIGINT
 wait $!          # Wait for the loop to exit, one way or another
 trap - SIGINT    # Remove the trap, now we're done with it
 
 echo "Waiting for kubernetes to finish installing, this will take a couple of minutes"
-until [ "`ssh kube1cp2.k8s.lan '[ -f /home/built ] && echo -n 1' 2> /dev/null`" == "1" ]
+until [ "`ssh kube1cp3.k8s.lan '[ -f /home/built ] && echo -n 1' 2> /dev/null`" == "1" ]
 do
     sleep 5;
     echo -n ".";
