08a15253c66c02057003424727935054a5378b91
Edward Cooke <edward@frakkingsweet.com>
2023-05-24 16:33:49 -0600

    vm_id should just be vm

--- dns/postbuild.sh
+++ dns/postbuild.sh
@@ -8,7 +8,7 @@ wait $!          # Wait for the loop to exit, one way or another
 trap - SIGINT    # Remove the trap, now we're done with it
 
 echo "Waiting for bind to be installed, this will take a couple of minutes"
-until [ "`ssh -i ~/.ssh/vm_id 192.168.122.254 '[ -f /home/built ] && echo -n 1' 2> /dev/null`" == "1" ]
+until [ "`ssh -i ~/.ssh/vm 192.168.122.254 '[ -f /home/built ] && echo -n 1' 2> /dev/null`" == "1" ]
 do
     sleep 5;
     echo -n ".";
